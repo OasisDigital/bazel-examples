270252cdc62886c0f0806c2be0ed9890db2e3b78
Kyle Cordes <kyle.cordes@oasisdigital.com>
2022-02-16 06:27:03 -0600

    remove a placeholder

--- 10-adding-language-support/README.md
+++
@@ -1,3 +0,0 @@
-# WORK IN PROGRESS
-
-(Not yet developed - for now, we look at existing off-the-shelf Bazel rules.)
--- 10-adding-language-support/exercise.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-exit
-
-bazel build '...'
-# bazel test '...' --test_verbose_timeout_warnings
